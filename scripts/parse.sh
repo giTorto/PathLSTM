3c84c3ff946bfa2fe78decde9a3a5ed924e56a5e
giTorto <giulian.trt@gmail.com>
2016-12-19 13:36:04 +0100

    Updated to create unique jar and easy the call from the scrip

--- scripts/parse.sh
+++ scripts/parse.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 # please download these models and adjust their locations accordingly
-LEMMA_MODEL=models/lemma-eng.model
-POS_MODEL=models/tagger-eng.model
-PARSER_MODEL=models/parse-eng.model
-SRL_MODEL=models/srl-ACL2016-eng.model
-
+LEMMA_MODEL=$1 #models/lemma-eng.model
+POS_MODEL=$2 #models/tagger-eng.model
+PARSER_MODEL=$3 #models/parse-eng.model
+SRL_MODEL=$4 #models/srl-ACL2016-eng.model
+INPUT_FILE_TO_TEST=$5
 RERANKER="-reranker -externalNNs"
 TOKENIZE="-tokenize"
 
 # java 1.8+ is required
-JAVA=/usr/lib/jvm/java-1.8.0/bin/java
+JAVA=$(which java)
 
-# parse $1
-$JAVA -Xmx60g se.lth.cs.srl.CompletePipeline eng -lemma $LEMMA_MODEL -parser $PARSER_MODEL -tagger $POS_MODEL -srl $SRL_MODEL $RERANKER $TOKENIZE -test $1
+# parse INPUT_FILE_TO_TEST
+$JAVA -Xmx16g  -jar PathLSTM/target/PathLSTM-1.0.0-jar-with-dependencies.jar  eng -lemma $LEMMA_MODEL -parser  $PARSER_MODEL -tagger $POS_MODEL -srl $SRL_MODEL $RERANKER $TOKENIZE -test $INPUT_FILE_TO_TEST
+
+# old way of calling the script
+#$JAVA -Xmx60g  se.lth.cs.srl.CompletePipeline eng -lemma $LEMMA_MODEL -parser $PARSER_MODEL -tagger $POS_MODEL -srl $SRL_MODEL $RERANKER $TOKENIZE -test $INPUT_FILE_TO_TEST
